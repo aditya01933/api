38bb61be2f29cf873881adcaa09173627a62fc42
Roman Rott <roman.rott@gmail.com>
2015-03-09 00:21:26 +0200

    configured token-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   #devise_for :users
-
+  mount_devise_token_auth_for 'User', at: 'auth'
   # token auth routes available at /api/v1/auth
-  namespace :api do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-    end
-  end
+  #namespace :api, defaults: { format: 'json' } do
+  #  scope :v1 do
+  #    mount_devise_token_auth_for 'User', at: 'auth'
+  #  end
+  #end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
